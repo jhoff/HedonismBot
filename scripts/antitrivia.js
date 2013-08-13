632ef679ee160b802a641c1576a9af565797d040
Jordan Hoff <jhoff@linkedin.com>
2013-08-13 15:54:23 -0500

    Fuck you Trivia Bot

---
+++ scripts/antitrivia.js
@@ -0,0 +1,21 @@
+//Description:
+//  Anti Trivia-bot script
+//
+//Dependencies:
+//  None
+//
+//Configuration:
+//  None
+//
+//Commands:
+//  None
+//
+//Author:
+//  jhoff
+
+module.exports = function(robot) {
+	robot.respond( /^!trivia .*/i, function(msg) {
+  	msg.send( "!trivia reset" );
+  	msg.send( "NO MORE GOD DAMN TRIVIA!" );
+	});
+}
