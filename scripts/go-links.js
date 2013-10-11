23481b1e6e19a3799d7559e661205d74d0c4a67f
Jordan Hoff <jhoff@linkedin.com>
2013-10-11 16:09:54 -0500

    go links

---
+++ scripts/go-links.js
@@ -0,0 +1,17 @@
+//Description:
+//  Converts go/blah into http://go/blah
+//
+//Dependencies:
+//  None
+//
+//Configuration:
+//  None
+//
+//Author:
+//  jhoff
+
+module.exports = function(robot) {
+  robot.hear( /[^\/]go\/[a-z\-_0-9]+/i, function(msg){
+    msg.send( 'http://' + msg.match[0] );
+  });
+}
