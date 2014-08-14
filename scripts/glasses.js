169b0aa5baeb6eac466119a171b1e13c25a549da
Matt Hackmann <matt.hackmann@gmail.com>
2014-08-14 09:41:12 -0700

    won't be fooled again

---
+++ scripts/glasses.js
@@ -0,0 +1,22 @@
+// Description:
+//   YEEEEEEEEEEEAAAAAAAAAAAAAAAHHHHHHHHHHHHHHH
+// 
+// Dependencies:
+//   None
+// 
+// Configuration:
+//   None
+// 
+// Commands:
+//   hubot stock <info|quote|price> for <ticker> - Get a stock price
+// 
+// Author:
+//   eliperkins
+
+module.exports = function(robot) {
+  robot.respond(/yeeaahh/i, function(msg) {
+    msg.send('(•_•)');
+    msg.send('( •_•)>⌐■-■');
+    msg.send('(⌐■_■)');
+  });
+};
